d33da7e08ac5ca576be09b6f8e9df0513f832e60
Marut Papao <marooter19959@gmail.com>
2025-12-04 15:04:59 +0700

    fix user permission

--- components.js
+++ components.js
@@ -89,13 +89,7 @@ function renderSidebar(activePageId) {
                 <span data-i18n="nav_service_requests">Service Requests</span>
             </a>` : ''}
 
-            ${hasPermission('analytics') ? `
-            <a href="#" class="${getLinkClass('analytics')}">
-                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                </svg>
-                <span data-i18n="nav_analytics">Analytics</span>
-            </a>` : ''}
+
 
             ${hasPermission('users') ? `
             <a href="users.html" class="${getLinkClass('users')}">
